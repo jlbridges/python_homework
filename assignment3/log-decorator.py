a0a468f48a85269548c3645340d9238750868e31
jacob bridges <jlbridges@my.waketech.edu>
2026-07-19 12:34:04 -0400

    lkdjlk;sdjlsdd

--- assignment3/log-decorator.py
+++ assignment3/log-decorator.py
@@ -1,28 +1,40 @@
 import logging
 
-
 logger = logging.getLogger(__name__ + "_parameter_log")
 logger.setLevel(logging.INFO)
-logger.addHandler(logging.FileHandler("./decorator.log","a"))
+logger.addHandler(logging.FileHandler("./decorator.log", "a"))
+
+
 def logger_decorator(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
+
+        positional = list(args) if args else 'none'
+        keyword = dict(kwargs) if kwargs else 'none'
+
         logger.log(logging.INFO, f"function: {func.__name__}")
-        logger.log(logging.INFO, f"positional parameters: {args}")
-        logger.log(logging.INFO, f"keyword parameters: {kwargs}")
+        logger.log(logging.INFO, f"positional parameters: {positional}")
+        logger.log(logging.INFO, f"keyword parameters: {keyword}")
         logger.log(logging.INFO, f"return: {result}")
         return result
     return wrapper
+
+
 @logger_decorator
 def hello():
     print("Hello, World!")
+
+
 @logger_decorator
 def new_func(*args):
     return True
+
+
 @logger_decorator
 def new_new_func(**kwargs):
-    return logger_decorator(hello)
+    return logger_decorator
+
 
 hello()
 new_func('test')
-new_new_func(test = 'test2')
+new_new_func(test='test2')
