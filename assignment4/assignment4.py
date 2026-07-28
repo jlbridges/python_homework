42b9882952dbfe8546b654b4949c8305a9a758fd
jacob bridges <jlbridges@my.waketech.edu>
2026-07-28 11:58:36 -0400

    finish task 4

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -43,3 +43,30 @@ print(more_employees.info(verbose=False))
 
 dirty_data = pd.read_csv('dirty_data.csv')
 print(dirty_data)
+
+clean_data = dirty_data.copy().drop_duplicates()
+print('data cleaned')
+print(clean_data)
+
+clean_data['Age'] = pd.to_numeric(clean_data['Age'],errors = "coerce")
+
+clean_data["Salary"] = clean_data["Salary"].replace("unknown", pd.NA)
+
+clean_data['Salary'] = pd.to_numeric(clean_data['Salary'],errors = "coerce")
+print('data even more cleaned ')
+print(clean_data)
+
+mean_score = clean_data['Age'].mean()
+clean_data['Age'] = clean_data['Age'].fillna(mean_score)
+
+median_score = clean_data['Salary'].median()
+clean_data["Salary"] = clean_data["Salary"].fillna(median_score)
+
+clean_data["Hire Date"] = pd.to_datetime(clean_data["Hire Date"], format ='mixed', errors="coerce")
+
+clean_data["Name"] = clean_data["Name"].str.strip().str.upper()
+
+clean_data["Department"] = clean_data["Department"].str.strip().str.upper()
+print('cleanest data ever')
+print(clean_data)
+
